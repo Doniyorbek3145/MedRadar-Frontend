b29e1d8a4cc84102076a9608be56c34e2636c297
IsmoilAbdujalilov <abdujalilovismoilgithub@gmail.com>
2024-08-21 11:10:35 +0500

    feat

--- src/models/Home/Home.tsx
+++ src/models/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { Card } from "antd";
 import { Breadcrumb } from "components";
 
 const Home = () => {
@@ -5,10 +6,39 @@ const Home = () => {
     { id: 1, title: "Главная", path: "/" },
     { id: 2, title: "Lorem ipsum dolor", path: "/" },
   ];
+
+  const gridStyle: React.CSSProperties = {
+    width: "20%",
+  };
   return (
-    <div>
-      <Breadcrumb paths={paths} className="bread-crumb" />
-      <h1>Home page</h1>
+    <div className="hero">
+      <div className="container">
+        <Breadcrumb paths={paths} className="bread-crumb" />
+        <h1 className="hero-title">Lorem ipsum dolor</h1>
+
+        <Card className="card">
+          <Card.Grid hoverable={false} style={gridStyle}>
+            <h2 className="card-title">Bemorlar</h2>
+            <h3 className="card-count">200</h3>
+          </Card.Grid>
+          <Card.Grid hoverable={false} style={gridStyle}>
+            <h2 className="card-title">Наличные:</h2>
+            <h3 className="card-count">1 470 000</h3>
+          </Card.Grid>
+          <Card.Grid hoverable={false} style={gridStyle}>
+            <h2 className="card-title">Терминал:</h2>
+            <h3 className="card-count">2 000 000</h3>
+          </Card.Grid>
+          <Card.Grid hoverable={false} style={gridStyle}>
+            <h2 className="card-title">Средняя цена продуктов</h2>
+            <h3 className="card-count">80 000</h3>
+          </Card.Grid>
+          <Card.Grid hoverable={false} style={gridStyle}>
+            <h2 className="card-title">Количество продуктов</h2>
+            <h3 className="card-count">45</h3>
+          </Card.Grid>
+        </Card>
+      </div>
     </div>
   );
 };
