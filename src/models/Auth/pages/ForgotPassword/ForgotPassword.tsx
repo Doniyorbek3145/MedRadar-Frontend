ed6fde63f9251741c93919c31317232c358d88d9
IsmoilAbdujalilov <abdujalilovismoilgithub@gmail.com>
2024-08-22 13:43:27 +0500

    feat

--- src/models/Auth/pages/ForgotPassword/ForgotPassword.tsx
+++ src/models/Auth/pages/ForgotPassword/ForgotPassword.tsx
@@ -1,9 +1,117 @@
-const ForgotPassword = () => {
+import type { FormProps } from "antd";
+import { Button, Form, Input } from "antd";
+import { ChangeEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { PhoneOutlined } from "@ant-design/icons";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  type authValueTypes = {
+    isPhone: boolean;
+  };
+
+  const [isAuthValue, setIsAuthValue] = useState<authValueTypes>({
+    isPhone: false,
+  });
+
+  const [authValue, setAuthValue] = useState<FieldType>({
+    phone: "",
+  });
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    setAuthValue({
+      ...authValue,
+      [e.target.name]: value,
+    });
+
+    if (e.target.name === "phone") {
+      if (e.target.value.length === 0) {
+        setIsAuthValue({
+          ...isAuthValue,
+          isPhone: true,
+        });
+      } else {
+        setIsAuthValue({
+          ...isAuthValue,
+          isPhone: false,
+        });
+      }
+    }
+  };
+
+  type FieldType = {
+    phone: string;
+  };
+
+  const onFinish: FormProps<FieldType>["onFinish"] = () => {};
+
+  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
+    errorInfo
+  ) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
-    <div>
-      <h1>ForgotPassword</h1>
-    </div>
+    <section className="auth-login">
+      <h1 className="visually-hidden">MedRadar Forgotpassword</h1>
+      <div className="auth-login__background">
+        <div className="auth-login__content">
+          <h2 className="auth-login__content-title">Parolni unitdingizmi?</h2>
+          <p className="auth-login__content-text">
+            Kiritgan telefon raqamingizga 4 xonali kod yuboriladi
+          </p>
+        </div>
+
+        <Form
+          name="basic"
+          autoComplete="off"
+          onFinish={onFinish}
+          className="auth-login__form"
+          onFinishFailed={onFinishFailed}
+          initialValues={{ remember: true }}
+        >
+          <Form.Item<FieldType>
+            name="phone"
+            className="auth-login__label"
+            rules={[
+              {
+                required: true,
+                message: "Iltimos telefon nomer ni kirgizing",
+              },
+            ]}
+          >
+            <Input
+              name="phone"
+              onChange={onChange}
+              value={authValue.phone}
+              className="auth-login__form-input"
+              placeholder="+998 ( _ _ ) _ _ _  _ _  _ _"
+              prefix={
+                <PhoneOutlined
+                  className={`auth-login__form-icon ${
+                    isAuthValue.isPhone && "auth-login__form-icon--color"
+                  }`}
+                />
+              }
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="auth-login__form-submit"
+            >
+              Kirish
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </section>
   );
 };
 
-export default ForgotPassword;
+export default Login;
--- src/models/Home/Home.tsx
+++ src/models/Home/Home.tsx
@@ -1,5 +1,6 @@
+import { Card } from "antd";
 import { Breadcrumb, Button } from "components";
-import { Card, message, Upload, UploadProps } from "antd";
+import { CloudDownloadOutlined } from "@ant-design/icons";
 
 const Home = () => {
   const paths = [
@@ -8,26 +9,9 @@ const Home = () => {
   ];
 
   const gridStyle: React.CSSProperties = {
-    width: "20%",
+    width: "33.33%",
   };
 
-  const props: UploadProps = {
-    name: "file",
-    action: "https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload",
-    headers: {
-      authorization: "authorization-text",
-    },
-    onChange(info) {
-      if (info.file.status !== "uploading") {
-        console.log(info.file, info.fileList);
-      }
-      if (info.file.status === "done") {
-        message.success(`${info.file.name} file uploaded successfully`);
-      } else if (info.file.status === "error") {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-    },
-  };
   return (
     <div className="hero">
       <div className="container">
@@ -38,18 +22,16 @@ const Home = () => {
 
           <ul className="hero__button-group">
             <li className="hero__button-item">
-              <Upload {...props}>
-                <Button className="hero__button-group-btn">
-                  Lorem ipsum dolor sit amet.
-                </Button>
-              </Upload>
+              <Button download className="hero__button-group-btn">
+                Lorem ipsum dolor sit amet.
+                <CloudDownloadOutlined className="hero__button-group-icon" />
+              </Button>
             </li>
             <li className="hero__button-item">
-              <Upload {...props}>
-                <Button className="hero__button-group-btn">
-                  Lorem ipsum dolor sit amet.
-                </Button>
-              </Upload>
+              <Button download className="hero__button-group-btn">
+                Lorem ipsum dolor sit amet.
+                <CloudDownloadOutlined className="hero__button-group-icon" />
+              </Button>
             </li>
           </ul>
         </div>
@@ -60,20 +42,12 @@ const Home = () => {
             <h3 className="card-count">200</h3>
           </Card.Grid>
           <Card.Grid hoverable={false} style={gridStyle}>
-            <h2 className="card-title">Наличные:</h2>
+            <h2 className="card-title">Patala</h2>
             <h3 className="card-count">1 470 000</h3>
           </Card.Grid>
           <Card.Grid hoverable={false} style={gridStyle}>
-            <h2 className="card-title">Терминал:</h2>
+            <h2 className="card-title">Kravat</h2>
             <h3 className="card-count">2 000 000</h3>
-          </Card.Grid>
-          <Card.Grid hoverable={false} style={gridStyle}>
-            <h2 className="card-title">Средняя цена продуктов</h2>
-            <h3 className="card-count">80 000</h3>
-          </Card.Grid>
-          <Card.Grid hoverable={false} style={gridStyle}>
-            <h2 className="card-title">Количество продуктов</h2>
-            <h3 className="card-count">45</h3>
           </Card.Grid>
         </Card>
 
